c7ed226315f425fedd31a741cf8706a7f787b0ce
olha <oh.semenovych@gmail.com>
2023-09-02 23:08:29 +0300

    resolve issue

--- src/main/java/todolist/controller/ToDoListErrorController.java
+++ src/main/java/todolist/controller/ToDoListErrorController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-
 public class ToDoListErrorController implements ErrorController {
 
     @RequestMapping("/error")
